277a21734626468c1bc8b6796e90af4946995a28
Gabriel Garzon <programaciongabriel2025@gmail.com>
2025-08-21 14:24:54 +0000

    He investigado y aparentemente hay que agregar el siguiente bloque de co

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,27 +2,21 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAMH8T42vojOtWAuC1MNHiCLds2J9KW0ps",
+  authDomain: "jardnia.firebaseapp.com",
+  projectId: "jardnia",
+  storageBucket: "jardnia.appspot.com",
+  messagingSenderId: "503843993979",
+  appId: "1:503843993979:web:3e217ea66688548147a5de"
 };
+
 
 // Initialize Firebase
 let app;
 if (!getApps().length) {
-  try {
-    if (!firebaseConfig.apiKey || firebaseConfig.apiKey.includes("PEGAR_AQUÍ")) {
-      throw new Error("Missing or placeholder Firebase API Key. Please check your .env file.");
-    }
-    app = initializeApp(firebaseConfig);
-  } catch (error) {
-    console.error("Error initializing Firebase. Check your firebaseConfig object and .env file.", error);
-    // You might want to handle this error more gracefully in a production app
-  }
+  app = initializeApp(firebaseConfig);
 } else {
   app = getApp();
 }
