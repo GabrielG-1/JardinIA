a10e7dff779f19f1ff22f8eddaa5b1926c40a683
Gabriel Garzon <programaciongabriel2025@gmail.com>
2025-08-24 18:56:29 +0000

    Ya no me muestra el catálogo, se dañó la página que teníamos

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 // These keys are public and safe to be stored here.
 // Security is managed by Firebase Security Rules.
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "TU_API_KEY_AQUI",
+  authDomain: "TU_AUTH_DOMAIN_AQUI",
+  projectId: "TU_PROJECT_ID_AQUI",
+  storageBucket: "TU_STORAGE_BUCKET_AQUI",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID_AQUI",
+  appId: "TU_APP_ID_AQUI",
 };
 
 // Initialize Firebase
