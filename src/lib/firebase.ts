d34cd48c5d84858184c39857ad288a13c6179535
Gabriel Garzon <programaciongabriel2025@gmail.com>
2025-08-24 19:04:33 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 // These keys are public and safe to be stored here.
 // Security is managed by Firebase Security Rules.
 const firebaseConfig = {
-  apiKey: "TU_API_KEY_AQUI",
-  authDomain: "TU_AUTH_DOMAIN_AQUI",
-  projectId: "TU_PROJECT_ID_AQUI",
-  storageBucket: "TU_STORAGE_BUCKET_AQUI",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID_AQUI",
-  appId: "TU_APP_ID_AQUI",
+  projectId: "jardnia",
+  appId: "1:503843993979:web:3e217ea66688548147a5de",
+  storageBucket: "jardnia.appspot.com",
+  apiKey: "AIzaSyAMH8T42vojOtWAuC1MNHiCLds2J9KW0ps",
+  authDomain: "jardnia.firebaseapp.com",
+  messagingSenderId: "503843993979",
 };
 
 // Initialize Firebase
