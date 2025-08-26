0e4b70a4b96184edc5d271c225a98150c0d7a8d9
Gabriel Garzon <programaciongabriel2025@gmail.com>
2025-08-26 05:19:36 +0000

    Funciona muy bien, el único detalle, es que arriba  la izquierda no se a

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -111,7 +111,7 @@ export default function AdminDashboardPage() {
   };
 
   return (
-    <div className="container mx-auto p-4 md:p-8 bg-muted/20 min-h-screen">
+    <div className="container mx-auto p-4 md:p-8 bg-muted/20 min-h-screen pt-28">
       <header className="flex flex-col sm:flex-row justify-between sm:items-center mb-8 gap-4">
         <div>
           <h1 className="text-3xl font-bold font-headline">Panel de Administrador</h1>
